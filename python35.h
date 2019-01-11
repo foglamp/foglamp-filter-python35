a2cd428add630424ffb8928f03085fc92e8a05f7
Massimiliano Pinto <massimiliano.pinto@gmail.com>
2019-01-11 11:40:40 +0100

    FOGL-2207: Initial implementation of Python35Filter derived class
    FOGL-2207: Initial implementation of Python35Filter derived class

---
+++ python35.h
@@ -0,0 +1,78 @@
+#ifndef _PYTHON35_FILTER_H
+#define _PYTHON35_FILTER_H
+/*
+ * FogLAMP "Python 3.5" filter class.
+ *
+ * Copyright (c) 2019 Dianomic Systems
+ *
+ * Released under the Apache 2.0 Licence
+ *
+ * Author: Massimiliano Pinto
+ */
+
+#include <mutex>
+
+#include <filter_plugin.h>
+#include <filter.h>
+
+#include <Python.h>
+
+// Relative path to FOGLAMP_DATA
+#define PYTHON_FILTERS_PATH "/scripts"
+
+/**
+ * Python35Filter class is derived from FogLampFilter
+ * It handles loading of a python module (provided script name)
+ * and the Python3.5 C API calls
+ */
+class Python35Filter : public FogLampFilter
+{
+	public:
+		Python35Filter(const std::string& name,
+			       ConfigCategory& config,
+			       OUTPUT_HANDLE* outHandle,
+			       OUTPUT_STREAM output) :
+			       FogLampFilter(name,
+					config,
+					outHandle,
+					output)
+		{
+			m_pModule = NULL;
+			m_pFunc = NULL;
+		};
+
+		// Set the additional path for Python3.5 Foglamp scripts
+		void	setFiltersPath(const std::string& dataDir)
+		{
+        		// Set FogLAMP dataDir + filters dir
+			m_filtersPath = dataDir + PYTHON_FILTERS_PATH;
+		}
+		const std::string&
+			getFiltersPath() const { return m_filtersPath; };
+		bool	getScriptName();
+		bool	configure();
+		bool	reconfigure(const std::string& newConfig);
+		void	lock() { m_configMutex.lock(); };
+		void	unlock() { m_configMutex.unlock(); };
+		void	logErrorMessage();
+		// Filtering methods for Reading objects
+		PyObject*
+			createReadingsList(const std::vector<Reading *>& readings);
+		std::vector<Reading *>*
+			getFilteredReadings(PyObject* filteredData);
+
+	public:
+		// Python 3.5 loaded filter module handle
+		PyObject*	m_pModule;
+		// Python 3.5 callable method handle
+		PyObject*	m_pFunc;
+		// Python 3.5  script name
+		std::string	m_pythonScript;
+
+	private:
+		// Scripts path
+		std::string	m_filtersPath;
+		// Configuration lock
+		std::mutex	m_configMutex;
+};
+#endif
