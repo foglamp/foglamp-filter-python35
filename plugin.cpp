0f15ae5d27f6e04ce3d593208141f03dbb7d38f9
Massimiliano Pinto <massimiliano.pinto@gmail.com>
2019-01-18 14:56:04 +0100

    FOGL-2320: always initialise Python 3.5 interpreter in plugin_init
    FOGL-2320: always initialise Python 3.5 interpreter in plugin_init

--- plugin.cpp
+++ plugin.cpp
@@ -120,16 +120,6 @@ PLUGIN_HANDLE plugin_init(ConfigCategory* config,
 							outHandle,
 							output);
 
-	// Check first we have a Python script to load
-	if (!pyFilter->setScriptName())
-	{
-		// Force disable
-		pyFilter->disableFilter();
-
-		// Return filter handle
-		return (PLUGIN_HANDLE)pyFilter;
-	}
-		
 	// Embedded Python 3.5 program name
 	wchar_t *programName = Py_DecodeLocale(config->getName().c_str(), NULL);
         Py_SetProgramName(programName);
@@ -149,6 +139,16 @@ PLUGIN_HANDLE plugin_init(ConfigCategory* config,
 	// Remove temp object
 	Py_CLEAR(pPath);
 
+	// Check first we have a Python script to load
+	if (!pyFilter->setScriptName())
+	{
+		// Force disable
+		pyFilter->disableFilter();
+
+		// Return filter handle
+		return (PLUGIN_HANDLE)pyFilter;
+	}
+		
 	// Configure filter
 	if (!pyFilter->configure())
 	{
